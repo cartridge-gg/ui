c679e59003640d1b8205fbbfc733f59e476dd219
bal7hazar <bal7hazar@proton.me>
2025-06-13 17:57:58 +0200

    fix: remove extra code

--- src/components/icons/state/money.tsx
+++ src/components/icons/state/money.tsx
@@ -35,13 +35,3 @@ export const MoneyIcon = memo(
 );
 
 MoneyIcon.displayName = "MoneyIcon";
-
-<svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path fill="white" />
-</svg>;
--- src/components/icons/state/seedling.tsx
+++ src/components/icons/state/seedling.tsx
@@ -35,13 +35,3 @@ export const SeedlingIcon = memo(
 );
 
 SeedlingIcon.displayName = "SeedlingIcon";
-
-<svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path fill="white" />
-</svg>;
