4270efe86d2d2591dba2e7547bed368d599c4064
RM3 <roger@studioavante.com>
2025-12-18 00:22:18 -0300

    chore: updated cards stories

--- src/components/modules/collectibles/card/card.stories.tsx
+++ src/components/modules/collectibles/card/card.stories.tsx
@@ -11,6 +11,7 @@ const meta: Meta<typeof CollectibleCard> = {
     layout: "padded",
   },
   args: {
+    icon: "https://static.cartridge.gg/presets/loot-survivor/icon.png",
     images: [
       "https://raw.githubusercontent.com/cartridge-gg/presets/refs/heads/main/configs/loot-survivor/cover.png",
     ],
@@ -23,7 +24,7 @@ const meta: Meta<typeof CollectibleCard> = {
 export default meta;
 type Story = StoryObj<typeof CollectibleCard>;
 
-export const Default: Story = {
+export const Collection: Story = {
   render: function Render(args) {
     const [selected, setSelected] = useState(false);
 
@@ -31,10 +32,187 @@ export const Default: Story = {
       <div className="flex gap-2">
         <CollectibleCard
           {...args}
+          icon="https://static.cartridge.gg/presets/loot-survivor/icon.png"
           totalCount={1}
           selected={selected}
           onSelect={() => setSelected(!selected)}
           onClick={selected ? () => setSelected(!selected) : undefined}
+        />
+      </div>
+    );
+  },
+};
+
+export const Beasts: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon="https://static.cartridge.gg/presets/loot-survivor/icon.png"
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x0158160018d590d93528995b340260e65aedd76d28a686e9daa5c4e8fad0c5dd/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x158160018d590d93528995b340260e65aedd76d28a686e9daa5c4e8fad0c5dd/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+          ]}
+          totalCount={1}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+        />
+      </div>
+    );
+  },
+};
+
+export const Beast: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon={undefined}
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x046da8955829adf2bda310099a0063451923f02e648cf25a1203aac6335cf0e4/0x00000000000000000000000000000000000000000000000000000000000105de/image",
+          ]}
+          title='"Grim Sun" Hippogriff'
+          totalCount={0}
+          listingCount={1}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={() => setSelected(!selected)}
+        />
+      </div>
+    );
+  },
+};
+
+export const GoldenToken: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon={undefined}
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x04f5e296c805126637552cf3930e857f380e7c078e8f00696de4fc8545356b1d/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x4f5e296c805126637552cf3930e857f380e7c078e8f00696de4fc8545356b1d/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+          ]}
+          title="Golden Token"
+          totalCount={0}
+          listingCount={0}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={() => setSelected(!selected)}
+        />
+      </div>
+    );
+  },
+};
+
+export const Karat: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon={null}
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x07d8ea58612a5de25f29281199a4fc1f2ce42f0f207f93c3a35280605f3b8e68/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x7d8ea58612a5de25f29281199a4fc1f2ce42f0f207f93c3a35280605f3b8e68/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+          ]}
+          title="Karat"
+          totalCount={1}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={selected ? () => setSelected(!selected) : undefined}
+        />
+      </div>
+    );
+  },
+};
+
+export const Bloberts: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon="https://static.cartridge.gg/presets/blob-arena-amma/icon.png"
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x00539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1/0x0000000000000000000000000000000000000000000000000000000000000001/image",
+          ]}
+          title="Bloberts"
+          totalCount={5}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={selected ? () => setSelected(!selected) : undefined}
+          backgroundColor="#C67927"
+        />
+      </div>
+    );
+  },
+};
+
+export const Duelists: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon="https://static.cartridge.gg/presets/pistols/icon.png"
+          images={[
+            "https://api.cartridge.gg/x/arcade-pistols/torii/static/0x07aaa9866750a0db82a54ba8674c38620fa2f967d2fbb31133def48e0527c87f/0x0000000000000000000000000000000000000000000000000000000000000577/image",
+            "https://api.cartridge.gg/x/arcade-pistols/torii/static/0x7aaa9866750a0db82a54ba8674c38620fa2f967d2fbb31133def48e0527c87f/0x0000000000000000000000000000000000000000000000000000000000000577/image",
+          ]}
+          title="Pistols at Dawn Duelists"
+          totalCount={1}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={selected ? () => setSelected(!selected) : undefined}
+          backgroundColor="#57493e"
+        />
+      </div>
+    );
+  },
+};
+
+export const Realms: Story = {
+  render: function Render(args) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <CollectibleCard
+          {...args}
+          icon="https://static.cartridge.gg/presets/eternum/icon.svg"
+          images={[
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x07ae27a31bb6526e3de9cf02f081f6ce0615ac12a6d7b85ee58b8ad7947a2809/0x0000000000000000000000000000000000000000000000000000000000001f40/image",
+            "https://api.cartridge.gg/x/arcade-main/torii/static/0x7ae27a31bb6526e3de9cf02f081f6ce0615ac12a6d7b85ee58b8ad7947a2809/0x0000000000000000000000000000000000000000000000000000000000001f40/image",
+          ]}
+          title="Realms"
+          totalCount={112}
+          selectable={true}
+          selected={selected}
+          onSelect={() => setSelected(!selected)}
+          onClick={selected ? () => setSelected(!selected) : undefined}
+          backgroundColor="#ffffff"
         />
       </div>
     );
@@ -61,157 +239,10 @@ export const Selectable: Story = {
   },
 };
 
-export const Beast: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x0158160018d590d93528995b340260e65aedd76d28a686e9daa5c4e8fad0c5dd/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x158160018d590d93528995b340260e65aedd76d28a686e9daa5c4e8fad0c5dd/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-          ]}
-          totalCount={1}
-          selectable={true}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
-export const Karat: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x07d8ea58612a5de25f29281199a4fc1f2ce42f0f207f93c3a35280605f3b8e68/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x7d8ea58612a5de25f29281199a4fc1f2ce42f0f207f93c3a35280605f3b8e68/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-          ]}
-          totalCount={1}
-          selectable={true}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
-export const GoldenToken: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x04f5e296c805126637552cf3930e857f380e7c078e8f00696de4fc8545356b1d/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-            "https://api.cartridge.gg/x/arcade-main/torii/static/0x4f5e296c805126637552cf3930e857f380e7c078e8f00696de4fc8545356b1d/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-          ]}
-          totalCount={1}
-          selectable={true}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
-export const Bloberts: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-ponziland-nft/torii/static/0x0539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-            "https://api.cartridge.gg/x/arcade-ponziland-nft/torii/static/0x539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-          ]}
-          totalCount={1}
-          selectable={true}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
-export const Duelists: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-pistols/torii/static/0x2e9c711b1a7e2784570b1bda5082a92606044e836ba392d2b977d280fb74b3c/0x0000000000000000000000000000000000000000000000000000000000000001/image",
-          ]}
-          totalCount={1}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
-export const Fallback: Story = {
-  render: function Render(args) {
-    const [selected, setSelected] = useState(false);
-
-    return (
-      <div className="flex gap-2">
-        <CollectibleCard
-          {...args}
-          images={[
-            "https://api.cartridge.gg/x/arcade-pistols/torii/static/0x07aaa9866750a0db82a54ba8674c38620fa2f967d2fbb31133def48e0527c87f/0x0000000000000000000000000000000000000000000000000000000000000577/image",
-            "https://api.cartridge.gg/x/arcade-pistols/torii/static/0x7aaa9866750a0db82a54ba8674c38620fa2f967d2fbb31133def48e0527c87f/0x0000000000000000000000000000000000000000000000000000000000000577/image",
-          ]}
-          totalCount={1}
-          selectable={true}
-          selected={selected}
-          onSelect={() => setSelected(!selected)}
-          onClick={selected ? () => setSelected(!selected) : undefined}
-        />
-      </div>
-    );
-  },
-};
-
 export const Unselectable: Story = {
   args: {
     selectable: false,
     totalCount: 2,
-    listingCount: 1,
-  },
-};
-
-export const Faded: Story = {
-  args: {
-    variant: "faded",
-    totalCount: 2,
-    listingCount: 2,
-    price: "$2",
-    lastSale: "$2",
   },
 };
 
@@ -226,17 +257,6 @@ export const Unselected: Story = {
   args: {
     selectable: true,
     selected: false,
-  },
-};
-
-export const Icon: Story = {
-  args: {
-    icon: "",
-    selected: false,
-    price: "$2",
-    lastSale: "$2",
-    totalCount: 2,
-    listingCount: 1,
   },
 };
 
